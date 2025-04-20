0d38773ed49d3da9f359f2d6f374737a337bdfaa
Mamata <mamatapatil7666@gmail.com>
2025-04-20 13:35:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+import CurrencyConvertor from './components/CurrencyConvertor'
+function App() {
+
+  return (
+    <>
+    <div className="min-h-screen bg-slate-400 flex flex-col items-center justify-center">
+      <div className='container'>
+
+    
+    <CurrencyConvertor/>
+    </div>
+    </div>
+     
+    </>
+  )
+}
+
+export default App
